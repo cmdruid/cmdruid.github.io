79f9b977c939f2dae53f666f7b7d7af0d868d88b
Christopher Scott <cscottdev@protonmail.com>
2021-03-25 19:38:55 -0700

    Fixed some bugs with the navbar.

--- static/components/navbar/navbar.js
+++ static/components/navbar/navbar.js
@@ -3,28 +3,39 @@ console.log("navbar.js loaded!");
 const username = 'cmdruid',
       reponame = 'cmdruid.github.io',
       endpoint = window.location.pathname.match(/[a-z0-9\-]*\/$/gi)[0],
-      fullpath = `https://github.com/${username}/${reponame}/tree/master${endpoint}`;
+      fullpath = `https://github.com/${username}/${reponame}/tree/master${endpoint}`,
+      cssURL   = 'https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.7/dist/semantic.min.css';
 
-async function loadNav() {
-  const main    = document.querySelector('.main'),
-        navbar  = await loadHTML('navbar.html'),
+async function main() {
+  const body    = document.querySelector('body'),
+        navbar  = loadHTML(await fetchHTML('navbar.html')),
         homeBtn = navbar.querySelector('#home-link'),
         ghBtn   = navbar.querySelector('#github-link');
-  if (!(main && navbar)) console.log("Elements failed to load!");
-  main.prepend(navbar);
-  main.style.marginTop = '5em';
+  if (!(body && navbar)) console.log("Elements failed to load!");
+  body.prepend(navbar);
   homeBtn.setAttribute('onclick', `location.href='./';`);
   ghBtn.setAttribute('onclick', `location.href='${fullpath}';`);
 }
 
-async function loadHTML(file) {
+async function fetchHTML(filename) {
   try { 
     let url = document.currentScript.src.match(/^.*\//)[0],
-        res = await fetch(url + file),
-        txt = await res.text();
-    if (!(res.ok && txt)) throw Error(`Failed with status: ${res.status}`);
-    return new DOMParser().parseFromString(txt, 'text/html').firstChild;
-  } catch(err) { console.error(err); }
+        res = await fetch(url + filename);
+    if (!res.ok) throw Error(`Failed with status: ${res.status}`); 
+    return await res.text();
+  } catch(err) { console.error(err); return ''; }
 }
 
-loadNav();
+function loadHTML(HTMLtxt) {
+  let tmp = document.createElement('template');
+  tmp.innerHTML = HTMLtxt.trim();
+  return tmp.content.firstElementChild;
+}
+
+function loadStyle(url) {
+  let head = document.querySelector('head');
+  head.prepend(loadHTML(`<link rel="stylesheet" type="text/css" href="${url}">`));
+}
+
+loadStyle(cssURL);
+main();
